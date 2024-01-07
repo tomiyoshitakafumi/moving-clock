7a924ce2eea6d53174103831df65be115f51246a
冨吉　隆史 <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-01-08 02:56:09 +0900

    add ex05

--- exercises-main-exercises/exercises/ch01/ex05/index.js
+++ exercises-main-exercises/exercises/ch01/ex05/index.js
@@ -5,4 +5,25 @@
     else { // 省略可能なelse節は、
         return -x; // 比較がfalseのときに実行される。
     } // 節に1文しかない場合は、中括弧は省略できる。
-} 
+}
+
+export function sum(array) { // 配列の要素の合計を計算する。
+    let sum = 0; // sumの初期値を0にする。
+    for(let x of array) { // 配列をループし、各要素をxに代入する。
+        sum += x; // sumに各要素の値を加算する。
+    } // ここでループが終わる。
+    return sum; // sumを返す。
+}
+export function factorial(n) { // 階乗を計算する関数。
+   
+    if(n<0) {
+        throw new Error("負の値が設定されています");
+    }
+    let product = 1; // 1からスタート。
+    while (n > 1) { // ()中の式がtrueの間は{}中の文を繰り返す。
+        product *= n; // product = product * n; の短縮表記。
+        n--; // n = n - 1 の短縮表記。
+    } // ループの最後。
+    return product; // 計算結果を返す
+    
+}
--- exercises-main-exercises/exercises/ch01/ex05/index.test.js
+++ exercises-main-exercises/exercises/ch01/ex05/index.test.js
@@ -1,4 +1,4 @@
-import { abs } from "./index.js";
+import { abs, sum, factorial } from "./index.js";
 
 // TypeScript の場合は以下:
 // import { abs, sum, factorial } from "./index.ts";
@@ -19,4 +19,27 @@ describe("math", () => {
   });
 
   // 以下に sum, factorial のテストを記載せよ
+  describe("sum", () => {
+    it("小数点を含む値合計値が正しいか", () => {
+      expect(sum([1,2,3.1])).toBe(6.1);
+    });
+
+    it("配列が空の場合は正しいか", () => {
+      expect(sum([])).toBe(0);
+    });
+  });
+
+  describe("factorial", () => {
+    it("4!は24となるか", () => {
+      expect(factorial(4)).toBe(24);
+    });
+
+    it("-1!はエラーとなるか", () => {
+      expect(() => factorial(-1)).toThrow("負の値が設定されています");
+    });
+
+    it("0!は1となるか", () => {
+      expect(factorial(0)).toBe(1);
+    });
+  });
 });
