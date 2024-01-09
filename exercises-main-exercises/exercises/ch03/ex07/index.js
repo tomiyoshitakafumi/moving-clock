b509a32c6b90a6c21a5b97ece57d821af4b80be4
冨吉　隆史 <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-01-09 22:44:26 +0900

    add ex07

---
+++ exercises-main-exercises/exercises/ch03/ex07/index.js
@@ -0,0 +1,14 @@
+export function equalArrays(a, b) {
+    if (a === b) return true; // 同一の配列は等しい。
+    if (a.length !== b.length) return false; // 長さの異なる配列は等しくない。
+    for (let i = 0; i < a.length; i++) { // すべての要素を巡回する。
+        if (a[i] !== b[i]) return false; // 1つでも違っていれば、等しくない。
+    }
+    return true; // すべて同じであれば、等しい。
+}
+
+let a = function () {
+    return 2;
+}
+
+console.log(equalArrays({}, {a}))
