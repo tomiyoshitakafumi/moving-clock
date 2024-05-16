da42139785383beee9d21cac65f5141966841456
takafumi tomiyoshi <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-05-16 23:47:20 +0900

    until 10

---
+++ exercises/ch11/ex10/index.js
@@ -0,0 +1,33 @@
+﻿export function getDays(year, month) {
+    let date = new Date(year, month, 0);
+    return date.getDate();
+}
+
+export function countWeekDays(from, until) {
+    let fromDate = new Date(from);
+    let untilDate = new Date(until);
+    if (fromDate > untilDate) {
+        throw new Error("エラー");
+    }
+    let count = 0;
+    for (let date = fromDate; date <= untilDate; date.setDate(date.getDate() + 1)) {
+        if (1 <= date.getDay() && date.getDay() <= 5) {
+            count++;
+        }
+    }
+    return count;
+}
+
+export function getDayofWeek(d, locale) {
+    let date = new Date(d);
+    return date.toLocaleDateString(locale, {weekday: 'long'});
+}
+
+export function getFirstDayOfLstMonth() {
+    let now = new Date();
+    let ms = now.getDate() * 24 * 60 * 60 * 1000 + now.getHours() * 60 * 60 * 1000 + now.getMinutes() * 60 * 1000 + now.getSeconds() * 1000 + now.getMilliseconds()
+    //先月の月末日0時0分0秒のDateオブジェクト
+    let date = new Date(now - ms);
+    //月末日の日付を引くことで先月1日0時0分0秒のDateオブジェクトとする ex:月末が30日なら29日の時間を引く
+    return new Date(date - (date.getDate() - 1) * 24 * 60 * 60 * 1000);
+}
---
+++ exercises/ch11/ex10/index.test.js
@@ -0,0 +1,27 @@
+﻿import { countWeekDays, getDayofWeek, getDays, getFirstDayOfLstMonth } from './index';
+// import { factorial } from "../../ch01/ex05/index.js";
+
+test('getDays', () => {
+    expect(getDays(2024, 5)).toBe(31);
+    //うるう年
+    expect(getDays(2024, 2)).toBe(29);
+    expect(getDays(2022, 1)).toBe(31);
+});
+
+test('countWeekDays', () => {
+    expect(countWeekDays('2024-05-01', '2024-05-31')).toBe(23);
+    expect(countWeekDays('2023-12-01', '2024-01-03')).toBe(24);
+    expect(() => countWeekDays('3000-01-01', '2024-01-01',)).toThrow("エラー");
+});
+
+test('getDayofWeek', () => {
+    expect(getDayofWeek('2024-05-16', 'en-US')).toBe('Thursday');
+    expect(getDayofWeek('2024-04-16', 'de-DE')).toBe('Dienstag');
+});
+
+test('getFirstDayOfLstMonth', () => {
+    let date = new Date(getFirstDayOfLstMonth());
+    let now = new Date();
+    expect(date).toEqual(new Date(now.getFullYear(), now.getMonth() - 1, 1));
+
+});
