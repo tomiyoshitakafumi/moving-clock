a0d749a920c973a986df302f7e4bc6239a26dafa
takafumi tomiyoshi <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-06-09 23:43:08 +0900

    until 06

---
+++ exercises/ch12/ex06/index.js
@@ -0,0 +1,25 @@
+﻿import fs from 'fs';
+import path from 'path';
+
+export function* walk(rootPath) {
+    let stat = fs.statSync(rootPath);
+
+    if (stat.isDirectory()) {
+        yield {path: path.join(rootPath, ''), isDirectory: true};
+
+        const files = fs.readdirSync(rootPath);
+        for (let file of files) {
+            //現在のpathをrootPathとfileで結合して再帰的に処理する
+            yield* walk(path.join(rootPath, file));
+        }
+    } else {
+        yield {path: rootPath, isDirectory: false};
+    }
+}
+
+//
+// let a = walk('./test');
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
---
+++ exercises/ch12/ex06/index.test.js
@@ -0,0 +1,11 @@
+﻿import { walk } from "./index";
+
+it("walk", () => {
+    let walkpath = walk('./ch12/ex06/test');
+    //windows環境の場合は、パスの区切り文字が'\\'になる
+    expect(walkpath.next().value).toEqual({path: 'ch12\\ex06\\test', isDirectory: true});
+    expect(walkpath.next().value).toEqual({path: 'ch12\\ex06\\test\\test2', isDirectory: true});
+    expect(walkpath.next().value).toEqual({path: 'ch12\\ex06\\test\\test2\\test3', isDirectory: true});
+    expect(walkpath.next().value).toEqual({path: 'ch12\\ex06\\test\\test2\\test4', isDirectory: true});
+    expect(walkpath.next().value).toEqual({path: 'ch12\\ex06\\test\\test2\\test4\\test.txt', isDirectory: false});
+});
