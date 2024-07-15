2279dc324ba6f39e7fbb4a80172101782caee43f
takafumi tomiyoshi <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-07-15 22:36:28 +0900

    until ch15 ex01

--- exercises/ch15.01-03/ex01/index.js
+++ exercises/ch15.01-03/ex01/index.js
@@ -4,6 +4,7 @@ const input = document.querySelector("#new-todo");
 
 form.addEventListener("submit", (e) => {
   // TODO: ここで form のイベントのキャンセルを実施しなさい (なぜでしょう？)
+    e.preventDefault(); // デフォルトでページがリロードされてしまうのでイベント本来の動作を止める
 
   // 両端からホワイトスペースを取り除いた文字列を取得する
   if (input.value.trim() === "") {
@@ -22,10 +23,25 @@ form.addEventListener("submit", (e) => {
 
   const toggle = document.createElement("input");
   // TODO: toggle が変化 (change) した際に label.style.textDecorationLine を変更しなさい
-
+    toggle.type = "checkbox";
+    toggle.addEventListener("change", () => {
+        if (toggle.checked) {
+            // チェックされたら取り消し線を引く
+            label.style.textDecorationLine = "line-through";
+        } else {
+            // チェックが外れたら取り消し線を消す
+            label.style.textDecorationLine = "none";
+        }
+    });
   const destroy = document.createElement("button");
   // TODO: destroy がクリック (click) された場合に elem を削除しなさい
+    destroy.textContent = "❌";
+    destroy.addEventListener("click", () => elem.remove());
 
+    // elem に toggle, label, destroy を追加
+    elem.appendChild(toggle);
+    elem.appendChild(label);
+    elem.appendChild(destroy)
   // TODO: elem 内に toggle, label, destroy を追加しなさい
   list.prepend(elem);
 });
