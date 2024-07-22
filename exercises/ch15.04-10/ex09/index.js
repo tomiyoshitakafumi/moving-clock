a18e12a1a461c7570917ed4c4b628b1e6a0bd347
Seiichi KONDO <seikichi@kmc.gr.jp>
2024-07-22 10:25:09 +0900

    Add 15.04-10

---
+++ exercises/ch15.04-10/ex09/index.js
@@ -0,0 +1,53 @@
+document.getElementById("image").addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  if (!file) {
+    return;
+  }
+
+  const img = new Image();
+  const reader = new FileReader();
+
+  reader.addEventListener("load", (e) => {
+    img.src = e.target.result;
+  });
+
+  img.addEventListener("load", () => {
+    const originalCanvas = document.getElementById("original");
+    const filteredCanvas = document.getElementById("filtered");
+    const originalCtx = originalCanvas.getContext("2d");
+    const filteredCtx = filteredCanvas.getContext("2d");
+
+    originalCanvas.width = img.width;
+    originalCanvas.height = img.height;
+    filteredCanvas.width = img.width;
+    filteredCanvas.height = img.height;
+
+    originalCtx.drawImage(img, 0, 0);
+
+    const imageData = originalCtx.getImageData(0, 0, img.width, img.height);
+    const data = imageData.data;
+
+    // グレースケールへの変換 (RGB を足して平均を取っている)
+    //
+    // ガウシアンフィルタを実装する場合はこの周辺のコードを変更しなさい
+    // imageData の中身はそのままに別の配列に結果を格納するとよい
+    // ```js
+    // const outputData = new Uint8ClampedArray(imageData.data.length);
+    //
+    // // TODO: ここで imageData.data を参照して outputData に結果を格納
+    //
+    // const outputImageData = new ImageData(outputData, img.width, img.height);
+    // filteredCtx.putImageData(outputImageData, 0, 0);
+    // ```
+    for (let i = 0; i < data.length; i += 4) {
+      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+      data[i] = avg;
+      data[i + 1] = avg;
+      data[i + 2] = avg;
+    }
+
+    filteredCtx.putImageData(imageData, 0, 0);
+  });
+
+  reader.readAsDataURL(file);
+});
