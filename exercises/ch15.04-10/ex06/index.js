2047bc70f6cc04244cc47f00652fc037f9b22d44
takafumi tomiyoshi <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-08-19 01:29:51 +0900

    until 06

--- exercises/ch15.04-10/ex06/index.js
+++ exercises/ch15.04-10/ex06/index.js
@@ -21,6 +21,45 @@ class TodoApp extends HTMLElement {
 
     this.form = this.shadowRoot.querySelector("#new-todo-form");
     // TODO: 残りを実装
+    this.input = this.shadowRoot.querySelector("#new-todo");
+  　this.list = this.shadowRoot.querySelector("#todo-list");
+    
+  
+    this.form.addEventListener("submit", (e) => {
+      const li = document.createElement("li");
+      const div = document.createElement("div");
+      div.className = "view";
+
+      const toggle = document.createElement("input");
+      toggle.className = "toggle";
+      toggle.type = "checkbox";
+
+      const label = document.createElement("label");
+      label.className = "content";
+
+      const destroy = document.createElement("button");
+      destroy.className = "destroy";
+      destroy.textContent = "❌";
+      e.preventDefault();
+      if (this.input.value.trim() === "") {
+        return;
+      }
+      const todo = this.input.value.trim();
+      this.input.value = "";
+
+      toggle.addEventListener("change", () => {
+        li.classList.toggle("completed", toggle.checked);
+      });
+      label.textContent = todo;
+      destroy.addEventListener("click", () => {
+        li.remove();
+      });
+      div.appendChild(toggle);
+      div.appendChild(label);
+      div.appendChild(destroy);
+      li.appendChild(div);
+      this.list.prepend(li);
+    });
   }
 }
 
