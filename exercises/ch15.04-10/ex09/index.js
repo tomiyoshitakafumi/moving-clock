5945a76fccebd24201cdc84a54b51141f15c21c8
takafumi tomiyoshi <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-08-20 01:29:17 +0900

    until 10

--- exercises/ch15.04-10/ex09/index.js
+++ exercises/ch15.04-10/ex09/index.js
@@ -32,21 +32,56 @@ document.getElementById("image").addEventListener("change", (event) => {
     // ガウシアンフィルタを実装する場合はこの周辺のコードを変更しなさい
     // imageData の中身はそのままに別の配列に結果を格納するとよい
     // ```js
-    // const outputData = new Uint8ClampedArray(imageData.data.length);
+    const outputData = new Uint8ClampedArray(imageData.data.length);
     //
-    // // TODO: ここで imageData.data を参照して outputData に結果を格納
+    // TODO: ここで imageData.data を参照して outputData に結果を格納
     //
-    // const outputImageData = new ImageData(outputData, img.width, img.height);
-    // filteredCtx.putImageData(outputImageData, 0, 0);
-    // ```
-    for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i] = avg;
-      data[i + 1] = avg;
-      data[i + 2] = avg;
+    // ガウシアンフィルタは5×5 sigma=1.3とした
+    // k = 1/256 * [1  4  6  4 1]
+    //             [4 16 24 16 4]
+    //             [6 24 36 24 6]
+    //             [4 16 24 16 4]
+    //             [1  4  6  4 1]
+    // 以下畳み込み計算を行う。
+    
+    const kernel = [[1, 4, 6, 4, 1], [4,16,24,16,4],[6,24,36,24,6],[4,16,24,16,4],[1,4 ,6 ,4,1]];
+    
+    for(let row = 0; row < img.height; row++) {
+      //iは始まりの画像のピクセルの位置を示す
+      let i = row * img.width * 4;
+      // 格納されている。ピクセルごとに4つの連続するバイトで RGBA順に格納される。
+      for(let col = 1; col < img.width-1; col++,i += 4) {
+        let r = 0;
+        let g = 0;
+        let b = 0;
+        // 5x5のカーネルを使って畳み込み計算を行う
+        for (let kr = 0; kr < 5; kr++) {
+          for (let kc = 0; kc < 5; kc++) {
+            let k = kernel[kr][kc];
+            //カーネルの中心からどのくらいずれているか
+            let c = i + (kr - 2) * img.width * 4 + (kc - 2) * 4;
+            r += data[c] * k;
+            g += data[c + 1] * k;
+            b += data[c + 2] * k;
+          }
+        }
+        outputData[i] = r / 256; // 256で割ることで正規化 赤
+        outputData[i + 1] = g / 256;// 緑
+        outputData[i + 2] = b / 256;// 青
+        outputData[i + 3] = data[i + 3];// 透明度は変化なし
+      }
     }
-
-    filteredCtx.putImageData(imageData, 0, 0);
+    const outputImageData = new ImageData(outputData, img.width, img.height);
+    filteredCtx.putImageData(outputImageData, 0, 0);
+    // ```
+    // for (let i = 0; i < data.length; i += 4) {
+    //   const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    //   data[i] = avg;
+    //   data[i + 1] = avg;
+    //   data[i + 2] = avg;
+    // }
+    //
+    // filteredCtx.putImageData(imageData, 0, 0);
   });
 
   reader.readAsDataURL(file);
--- exercises/ch15.04-10/ex10/index.js
+++ exercises/ch15.04-10/ex10/index.js
@@ -47,6 +47,40 @@ function updateGrid(grid) {
   for (let row = 0; row < ROWS; row++) {
     for (let col = 0; col < COLS; col++) {
       // 周囲のセルの生存数を数えて nextGrid[row][col] に true or false を設定する (実装してね)
+      let count = 0;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          // 自分自身はカウントしない
+          if (i === 0 && j === 0) {
+            continue;
+          }
+          let newRow = row + i;
+          let newCol = col + j;
+          // 範囲外の場合はスキップ
+          if (newRow < 0 || ROWS <= newRow || newCol < 0 || COLS <= newCol) {
+            continue;
+          }
+          if (grid[newRow][newCol]) {
+            count++;
+          }
+        }
+      }
+      //誕生
+      if (!grid[row][col] && count === 3) {
+        nextGrid[row][col] = true;
+      }
+      //生存
+      else if (grid[row][col] && (count === 2 || count === 3)) {
+        nextGrid[row][col] = true;
+      }
+      //過疎
+      else if (grid[row][col] && count <= 1) {
+        nextGrid[row][col] = false;
+      }
+      //過密  
+      else if (grid[row][col] && count >= 4) {
+        nextGrid[row][col] = false;
+      }
     }
   }
   return nextGrid;
