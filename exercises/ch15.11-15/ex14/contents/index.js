646a319c0cfbea67ecad6d176faffcd2c1e5f2b8
nfukasawa <get.it.on.fuka@gmail.com>
2024-08-29 09:42:25 +0900

    add ch15.11-15

---
+++ exercises/ch15.11-15/ex14/contents/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const button = document.querySelector("#send-button");
+const messageContainer = document.getElementById("message-container");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  getMessageFromServer();
+});
+async function getMessageFromServer() {
+  const messageElement = document.createElement("div");
+  messageElement.className = "message";
+  messageElement.textContent = "";
+  messageContainer.appendChild(messageElement);
+
+  // TODO: ここにサーバーとのやり取り等を実装しなさい
+}
