644b6190cfdc71d4674e06563943f53255e9cc1a
tomi <Takafumi.Tomiyoshi@jp.ricoh.com>
2024-11-17 20:00:43 +0900

    until ex01

--- exercises/ch17/ex01/eslint.config.mjs
+++ exercises/ch17/ex01/eslint.config.mjs
@@ -1,10 +1,23 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-
+import googleConfig from "eslint-config-google";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginReact.configs.flat.recommended,
+  {
+    ...googleConfig,
+    rules: {
+      ...googleConfig.rules,
+      "valid-jsdoc": "off",
+      "require-jsdoc": "off"
+    }
+  },
 ];
+//Google JavaScript スタイルガイドのeslitnがあったので使ってみた。(jsdocを無効化にしないとダメだったが)
+
+///home/tomiyoshi/source/RICOH-JStraining/exercises/ch17/ex01/lint_sample.js
+// 4: 1  error  Parsing error: 'with' in strict mode
+// ✖ 1 problem(1 error, 0 warnings)
